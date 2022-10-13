a0398cd0ba32cc06b397f627856979d1a25a5ff3
vishnukumar07 <vishnuk55265@gmail.com>
2022-10-13 17:40:35 +0530

    last

--- App.js
+++ App.js
@@ -6,45 +6,7 @@ import {
   TouchableOpacity,
   View,
 } from "react-native";
-
+import Login from "./Components/Login";
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <TextInput
-        placeholder={"Enter the Username"}
-        style={{ height: 42, width: "80%", borderBottomWidth: 1 }}
-      />
-
-      <TextInput
-        placeholder={"Enter the password"}
-        style={{ height: 42, width: "80%", borderBottomWidth: 1 }}
-      />
-      <TouchableOpacity
-        style={{
-          borderWidth: 1,
-          height: 42,
-          width: "70 %",
-          justifyContent: "center",
-          alignItems: "center",
-          borderRadius: 20,
-          backgroundColor: "black",
-          marginTop: "4%",
-          alignSelf: "center",
-        }}
-      >
-        <Text style={{ color: "white" }}>Login</Text>
-      </TouchableOpacity>
-    </View>
-  );
+  return <Login />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    width: "100%",
-    height: "100%",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
