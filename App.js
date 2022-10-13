aa8410e826bff42714f596a1f9472530d7c3a1de
vishnukumar07 <vishnuk55265@gmail.com>
2022-10-13 17:20:25 +0530

    l

--- App.js
+++ App.js
@@ -1,11 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TextInput
+        placeholder={"Enter the Username"}
+        style={{ height: 42, width: "80%", borderBottomWidth: 1 }}
+      />
+
+      <TextInput
+        placeholder={"Enter the password"}
+        style={{ height: 42, width: "80%", borderBottomWidth: 1 }}
+      />
+      <TouchableOpacity
+        style={{
+          borderWidth: 1,
+          height: 42,
+          width: "70 %",
+          justifyContent: "center",
+          alignItems: "center",
+          borderRadius: 20,
+          backgroundColor: "black",
+          marginTop: "4%",
+          alignSelf: "center",
+        }}
+      >
+        <Text style={{ color: "white" }}>Login</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -13,8 +41,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    width: "100%",
+    height: "100%",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
